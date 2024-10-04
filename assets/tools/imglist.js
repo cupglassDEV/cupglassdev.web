8aaa823002118260e9f7e12afd3a1e6ccc621dce
cupglassDEV <vincmatheo@gmail.com>
2024-10-04 18:45:06 +0700

    ok aja lah, pasrah gua

--- assets/tools/imglist.js
+++ assets/tools/imglist.js
@@ -8,6 +8,8 @@ btw feel free to use this script on your esm based javascript
  */
 
 export default [
+    ["", "Lagi disorotin di Timedoor",
+        "wong fiturnya blm banyak"],
     ["profile.png", "HALOOO", 
         "Yang eliepsi gosah masuk ke website ini. Matanya entar joged loh"],
     ["https://upload.wikimedia.org/wikipedia/commons/6/65/COLLECTIE_TROPENMUSEUM_Kinderen_krijgen_les_op_een_lagere_school_van_de_stichting_Bhakti_Luhur_TMnr_20018375.jpg", "orang pramuka", 
